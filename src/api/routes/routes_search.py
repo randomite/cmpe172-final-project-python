44bdb3dbf5dab408640b27b959161384388b6e4f
randomite <ravin.sardal@gmail.com>
2018-11-27 19:42:29 -0800

    MYsql

--- src/api/routes/routes_search.py
+++ src/api/routes/routes_search.py
@@ -1,13 +1,28 @@
 from flask import Blueprint
 from api.utils import responses as resp
 from api.utils.responses import response_with
-from api.utils.sql import sql
+import pymysql
 
 route_search = Blueprint("route_search", __name__)
 
 
 @route_search.route("/search", methods=["GET"])
 def search():
-    s = sql()
-    resp = s.query(sql="SELECT * from employees")
+    # resp = s.query(sql="SELECT * from employees")
+    # Open database connection
+    #RootRoot
+    db = pymysql.connect(host='localhost', port=3306, user='root', passwd='RootRoot', db='employees')
+
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # execute SQL query using execute() method.
+    cursor.execute("SELECT VERSION()")
+
+    # Fetch a single row using fetchone() method.
+    data = cursor.fetchone()
+    print("Database version : %s " % data)
+
+    # disconnect from server
+    db.close()
     return response_with(resp.SUCCESS_200, value={"data": "yo"})
