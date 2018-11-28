55dac241c860bcad5fa293bd4c884bd9736520b0
Raj Makda <makdaraj7@gmail.com>
2018-11-27 23:52:15 -0800

    Adding authenthication using Auth0

--- src/api/utils/factory.py
+++ src/api/utils/factory.py
@@ -6,15 +6,40 @@ import sys
 from flask import Flask
 from flask import jsonify
 from flask_swagger import swagger
-from api.utils.database import db
 from api.utils.responses import response_with
 import api.utils.responses as resp
 from api.routes.routes_search import route_search
 
+from functools import wraps
+import json
+from os import environ as env
+
+from dotenv import load_dotenv, find_dotenv
+from flask import redirect
+from flask import render_template
+from flask import session
+from flask import url_for
+from authlib.flask.client import OAuth
+from six.moves.urllib.parse import urlencode
+
+import constants
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+AUTH0_CALLBACK_URL = env.get(constants.AUTH0_CALLBACK_URL)
+AUTH0_CLIENT_ID = env.get(constants.AUTH0_CLIENT_ID)
+AUTH0_CLIENT_SECRET = env.get(constants.AUTH0_CLIENT_SECRET)
+AUTH0_DOMAIN = env.get(constants.AUTH0_DOMAIN)
+AUTH0_BASE_URL = 'https://' + AUTH0_DOMAIN
+AUTH0_AUDIENCE = env.get(constants.AUTH0_AUDIENCE)
+if AUTH0_AUDIENCE is '':
+    AUTH0_AUDIENCE = AUTH0_BASE_URL + '/userinfo'
 
 def create_app(config):
-    app = Flask(__name__)
-
+    app = Flask(__name__,static_url_path='/public', static_folder='./public')
+    app.secret_key = constants.SECRET_KEY
     app.config.from_object(config)
     app.register_blueprint(route_search, url_prefix="")
 
@@ -37,6 +62,65 @@ def create_app(config):
     def not_found(e):
         logging.error(e)
         return response_with(resp.NOT_FOUND_HANDLER_404)
+
+    oauth = OAuth(app)
+
+    auth0 = oauth.register(
+        'auth0',
+        client_id=AUTH0_CLIENT_ID,
+        client_secret=AUTH0_CLIENT_SECRET,
+        api_base_url=AUTH0_BASE_URL,
+        access_token_url=AUTH0_BASE_URL + '/oauth/token',
+        authorize_url=AUTH0_BASE_URL + '/authorize',
+        client_kwargs={
+            'scope': 'openid profile',
+        },
+    )
+
+    def requires_auth(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            if constants.PROFILE_KEY not in session:
+                return redirect('/login')
+            return f(*args, **kwargs)
+
+        return decorated
+
+    # Controllers API
+    @app.route('/')
+    def home():
+        return render_template('home.html')
+
+    @app.route('/callback')
+    def callback_handling():
+        auth0.authorize_access_token()
+        resp = auth0.get('userinfo')
+        userinfo = resp.json()
+
+        session[constants.JWT_PAYLOAD] = userinfo
+        session[constants.PROFILE_KEY] = {
+            'user_id': userinfo['sub'],
+            'name': userinfo['name'],
+            'picture': userinfo['picture']
+        }
+        return redirect('/dashboard')
+
+    @app.route('/login')
+    def login():
+        return auth0.authorize_redirect(redirect_uri=AUTH0_CALLBACK_URL, audience=AUTH0_AUDIENCE)
+
+    @app.route('/logout')
+    def logout():
+        session.clear()
+        params = {'returnTo': url_for('home', _external=True), 'client_id': AUTH0_CLIENT_ID}
+        return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
+
+    @app.route('/dashboard')
+    @requires_auth
+    def dashboard():
+        return render_template('dashboard.html',
+                               userinfo=session[constants.PROFILE_KEY],
+                               userinfo_pretty=json.dumps(session[constants.JWT_PAYLOAD], indent=4))
 
     # END GLOBAL HTTP CONFIGURATIONS
 
---
+++ src/server.py
@@ -0,0 +1,96 @@
+# /server.py
+
+from functools import wraps
+import json
+from os import environ as env
+from werkzeug.exceptions import HTTPException
+
+from dotenv import load_dotenv, find_dotenv
+from flask import Flask
+from flask import jsonify
+from flask import redirect
+from flask import render_template
+from flask import session
+from flask import url_for
+from authlib.flask.client import OAuth
+from six.moves.urllib.parse import urlencode
+
+app = Flask(__name__)
+
+oauth = OAuth(app)
+
+auth0 = oauth.register(
+    'auth0',
+    client_id='3N6er5T0OiQlf3yX3iGsKRSBp2QoYccf',
+    client_secret='GLLrnkPPp85zJGX6HCjFho3ZCdh05QpeRnDpdKobULsUCNQTgZm--qCElungcjIv',
+    api_base_url='https://cmpe172team404.auth0.com',
+    access_token_url='https://cmpe172team404.auth0.com/oauth/token',
+    authorize_url='https://cmpe172team404.auth0.com/authorize',
+    client_kwargs={
+        'scope': 'openid profile',
+    },
+)
+
+# /server.py
+
+# Here we're using the /callback route.
+@app.route('/callback')
+def callback_handling():
+    # Handles response from token endpoint
+    auth0.authorize_access_token()
+    resp = auth0.get('userinfo')
+    userinfo = resp.json()
+
+    # Store the user information in flask session.
+    session['jwt_payload'] = userinfo
+    session['profile'] = {
+        'user_id': userinfo['sub'],
+        'name': userinfo['name'],
+        'picture': userinfo['picture']
+    }
+    return redirect('/dashboard')
+
+# /server.py
+
+@app.route('/login')
+def login():
+    return auth0.authorize_redirect(redirect_uri='/home', audience='https://cmpe172team404.auth0.com/userinfo')
+
+# /server.py
+
+def requires_auth(f):
+  @wraps(f)
+  def decorated(*args, **kwargs):
+    if 'profile' not in session:
+      # Redirect to Login page here
+      return redirect('/')
+    return f(*args, **kwargs)
+
+  return decorated
+
+# /server.py
+
+@app.route('/dashboard')
+@requires_auth
+def dashboard():
+    return render_template('dashboard.html',
+                           userinfo=session['profile'],
+                           userinfo_pretty=json.dumps(session['jwt_payload'], indent=4))
+
+# /server.py
+
+@app.route('/logout')
+def logout():
+    # Clear session stored data
+    session.clear()
+    # Redirect user to logout endpoint
+    params = {'returnTo': url_for('home', _external=True), 'client_id': '3N6er5T0OiQlf3yX3iGsKRSBp2QoYccf'}
+    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
+
+if __name__ == "__main__":
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    # sess.init_app(app)
+
+app.debug = True
+app.run()
